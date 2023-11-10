324b126022791b923d8daf08f2791ad2793e5517
davidGBbr <davidgomesbrigido@gmail.com>
2023-11-10 19:02:04 -0300

    Configurando test

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+//Criar um bloco que agrupa vários testes relacionados
+describe("First test app component", () => {
+  it("should adds 1 + 2 to equal 3", () => {
+    expect(1).toBe(1);
+  });
+});
+
+export default {};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>React + Testes</h1>
+      <p>teste</p>
+    </div>
+  );
+};
+
+export default App;
